1f1b6edf5877b81b691aa0b00acf76b2ba2bce7f
Iman rb <iman.rezasefat@gmail.com>
2025-03-06 13:13:22 +0330

    add translations to project flow

--- src/core/translation/locales/en/translation.ts
+++ src/core/translation/locales/en/translation.ts
@@ -1,7 +1,7 @@
 import general from './general.json';
 import layout from './layout.json';
 import verification from './verification.json';
-
+import projects from './projects.json';
 export function generateTranslationFile() {
-  return Object.assign({}, layout, general, verification);
+  return Object.assign({}, layout, general, verification, projects);
 }
